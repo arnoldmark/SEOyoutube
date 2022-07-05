5b93dd54ec87a572bcab0cd5a89dac1f137697e2
Mark Karomo <mk772@cairn.edu>
2022-07-05 19:12:13 +0000

    fixed api leak

--- SEOyoutubeapi.py
+++ SEOyoutubeapi.py
@@ -4,8 +4,7 @@ import json
 import os
 
 
-#api_key = os.environ.get('ytapi_key')
-api_key = 'AIzaSyBHJLFrB83J3C6NJkWTiPzVHOZjIydWjOc'
+api_key = os.environ.get("ytapi_key")
 channel_Id = 'UC4tjY2tTltEKePusozUxtSA'
 
 url = f'https://www.googleapis.com/youtube/v3/channels?part=contentDetails,statistics&id={channel_Id}&key={api_key}'
@@ -13,4 +12,10 @@ url = f'https://www.googleapis.com/youtube/v3/channels?part=contentDetails,stati
 json_url = requests.get(url)
 
 data = json.loads(json_url.text)
+
+try:
+    data = data["items"][0]["statistics"]
+except:
+    data = None
+
 print(data)
